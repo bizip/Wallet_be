f80de8ec9973f4ff79d1c2dc46adfbe54cc9ebce
Bizimungu Pascal <bizip04@gmail.com>
2025-01-13 23:39:01 +0200

    integrate Firebase Admin SDK for project backend
    -Added Firebase Admin SDK to the project for backend integration.
    -Configured Firebase initialization with service account credentials.
    -Created a reusable db module to interact with Firestore.
    
    [Done]

--- src/app.js
+++ src/app.js
@@ -1,20 +1,41 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+const { initializeApp, cert } = require("firebase-admin/app");
 
+dotenv.config();
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: "googleapis.com",
+};
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+initializeApp({
+    credential: cert(serviceAccount),
+    storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+});
+
 const start = () => {
     try {
-      app.listen({ port: PORT }, () =>
-        process.stdout.write(`http://localhost:${PORT} \n`)
-      );
+        app.listen({ port: PORT }, () =>
+            process.stdout.write(`http://localhost:${PORT} \n`)
+        );
     } catch (error) {
-      console.error(error.message);
-      process.exit(1);
+        console.error(error.message);
+        process.exit(1);
     }
-  };
-  
-  start();
+};
+
+start();
